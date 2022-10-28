0fd19e2deb344fbb5237e6b5db9212af2ea4ee5c
Marc Arque <marque@runroom.com>
2022-10-28 16:26:36 +0200

    Reestructure folders

---
+++ galleria-slideshow-site/src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+  import logo from './assets/logo.svg'
+</script>
+
+<div class="header">
+  <div class="header__logo">
+    <Logo />
+  </div>
+  <div class="header__button">
+    <button on:click={() => alert('Ding!')}>
+  </div>
+</div>

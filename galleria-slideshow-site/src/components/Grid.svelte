8c1268aea3eba6ffe682e9115266a06f18d53243
Imar Abreu <abreudiazimar9620@gmail.com>
2022-10-28 17:10:21 +0200

    Add data loader

---
+++ galleria-slideshow-site/src/components/Grid.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let images;
+</script>
+
+<div class="layout">
+  {#each images as image}
+  <div class="layout__item">
+    <img src="{image.src}" alt=""/>
+  </div>
+  {/each}
+</div>

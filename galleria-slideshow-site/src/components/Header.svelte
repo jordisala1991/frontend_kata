86646d94d0521ac4b29a5388e657b81df5744a8c
Marc Arque <marque@runroom.com>
2022-10-28 17:02:49 +0200

    Build header

--- galleria-slideshow-site/src/components/Header.svelte
+++ galleria-slideshow-site/src/components/Header.svelte
@@ -1,12 +1,8 @@
-<script>
-  import logo from './assets/logo.svg'
-</script>
-
 <div class="header">
   <div class="header__logo">
-    <Logo />
+    <img src="Logo.svg">
   </div>
   <div class="header__button">
-    <button on:click={() => alert('Ding!')}>
+    <button on:click={() => alert('Ding!')}>START SLIDESHOW</button>
   </div>
 </div>

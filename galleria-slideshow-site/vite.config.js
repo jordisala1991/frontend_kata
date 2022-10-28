7db74af849a79bbb4f8749347be3bcece9d8ceb1
Imar Abreu <abreudiazimar9620@gmail.com>
2022-10-28 16:21:37 +0200

    Add sass

--- galleria-slideshow-site/vite.config.js
+++ galleria-slideshow-site/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte({
+    preprocess: [sveltePreprocess({
+      sass: true
+    })]
+  })]
 })
